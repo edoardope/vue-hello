988464eee6a31efc199075bf392cc5fb8bba7d14
edoardope <pencoedoardo@gmail.com>
2023-04-19 01:32:39 +0200

    finito

---
+++ main.js
@@ -0,0 +1,16 @@
+const { createApp } = Vue  
+
+
+createApp({
+  data() {  
+    return {
+      titolo: "ciao vue!",
+      img: "https://picsum.photos/200"
+
+    }
+  },
+  methods: {
+      
+  }
+
+}).mount('#app')
